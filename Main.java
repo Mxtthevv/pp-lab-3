112b9f64472dd5bf24fa3223de625f38d5f88993
Mateusz, Matthew <122643510+Mxtthevv@users.noreply.github.com>
2024-04-07 11:47:04 +0200

    Update and rename main.java to Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(10.1, 5.7);
+
+        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
