db80b351ad96cec1d961b6369147047f3a1bd67b
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-04-07 11:48:30 +0200

    main.java

--- main.java
+++ main.java
@@ -1,1 +1,7 @@
-public
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(10.1, 5.7);
+
+        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
